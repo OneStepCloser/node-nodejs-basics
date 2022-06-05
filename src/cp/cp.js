79c638362be5c7f58a65a2416fa6b97ac655e007
OneStepCloser <artinspira@gmail.com>
2022-06-05 23:08:57 +0300

    Solve child process

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,3 +1,5 @@
+import { spawn } from 'child_process';
+
 export const spawnChildProcess = async (args) => {
-    // Write your code here
+    spawn('node', ['./files/script.js', ...args], { stdio: [process.stdin, process.stdout, process.stderr] });
 };
